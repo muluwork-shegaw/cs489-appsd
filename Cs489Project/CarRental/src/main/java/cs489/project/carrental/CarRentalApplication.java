8b76c87f8f88e97ad33b62fac8e112f579ae2cfc
muluwork-shegaw <smslmuluwork@gmail.com>
2024-04-10 14:09:15 -0500

    initalize the project structure add basic dependencies setup application.properties boiler points

---
+++ Cs489Project/CarRental/src/main/java/cs489/project/carrental/CarRentalApplication.java
@@ -0,0 +1,13 @@
+package cs489.project.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalApplication.class, args);
+    }
+
+}
